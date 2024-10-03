e43f03e99666d7991168dd65095743f7adc52230
Aptem Maleev <73392934+aptemmaleev@users.noreply.github.com>
2024-10-03 16:24:14 +0300

    Update server.py

--- app/api/server.py
+++ app/api/server.py
@@ -4,7 +4,18 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from app.api.routes import main_router
 from app.api.middlewares import AuthMiddleware
 
+from app.settings import SETTINGS
+from app.utils.mongodb import MongoDB
+
 
 app = FastAPI()
 app.add_middleware(BaseHTTPMiddleware, dispatch=AuthMiddleware())
 app.include_router(main_router)
+
+
+@app.on_event("startup")
+async def app_startup():
+    # Setup MongoDB
+    MongoDB.setup(
+        SETTINGS.MONGODB_URL.get_secret_value(), SETTINGS.MONGODB_DB.get_secret_value()
+    )
